e44fe4dbf5e49236fe8dfb6ef75f0583f24b952b
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-11-02 18:52:02 -0500

    Added an amepty array and password variable to generatePassword function. Also added first conditon statement for lowerCase criteria

--- script.js
+++ script.js
@@ -11,27 +11,35 @@ var numbers = confirm("would you like numbers?");
 alert("Ok great! so your password will have will contain: \n Length: " + length + " characters" + " \n Special Characters: "
   + specialChar + "\n Uppercase Letters: " + upperCase + "\n Lowercase Letters: " + lowerCase + "\n Numbers: " + numbers);
 
-  
-    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
+  //Criteria arrays
+    var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
     "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 
-    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    var numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-    var specialChar = ["!","#","$","&", "’", "(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+    var specialCharArray = ["!","#","$","&", "’", "(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
   
+    
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function generatePassword(options,length,lower){
+function generatePassword(length,upper,lower,numbers,special,upperArray,lowerArray,numbersArray,specialArray){
+var passwordArray = [];
+var password ="";
+
+if(lower){
+  password = password.concat(lowerArray[Math.floor(Math.random()*(lowerArray.length-1))]);
+  passwordArray = passwordArray.concat(lowerArray);
+}
 
 }
 
 function writePassword() {
-  var password = generatePassword(passwordOptions,length,lowerCase);
+  var password = generatePassword(length,upperCase,lowerCase,numbers,specialChar,upperCaseArray,lowerCaseArray,numbersArray,specialCharArray);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
